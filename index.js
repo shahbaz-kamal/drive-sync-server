50f890e7774452e165c9241ba0bc91f2de574aab
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-03-28 22:40:27 +0600

    login functionality implemented

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
 const port = process.env.PORT || 5000;
+const session = require("express-session");
 require("dotenv").config();
 
 const app = express();
+// express session middleware
+app.use(
+  session({
+    secret: process.env.SECRET, // Replace with a strong secret
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Use `true` in production with HTTPS
+  })
+);
+
 // middleware
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", async (req, res) => {
   res.send("driveSync server is running");
@@ -50,6 +67,20 @@ app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
-app.get("/auth/google/callback", { failureRedirect: "/" }, async (req, res) => {
-  res.redirect("/");
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: process.env.FRONTEND_URL,
+  }),
+  async (req, res) => {
+    res.redirect(process.env.FRONTEND_URL);
+  }
+);
+// sending user info to front end
+app.get("/auth/user", async (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.json(null);
+  }
 });
